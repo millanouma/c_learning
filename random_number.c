0af5072204dd9aff2ef1338f0dc69575136ca393
Millan Ouma <26522016+millanouma@users.noreply.github.com>
2023-05-20 23:52:51 +0300

    Update random_number.c
    added a 'function' way to do it.

--- random_number.c
+++ random_number.c
@@ -9,3 +9,22 @@ int main(void) {
   int randomNumber = rand() % 20 + 1;
   printf("%d\n", randomNumber);
 }
+
+#Another way to do it using functions:
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Modify the code below
+int getRandomNumber(int maxNumber) {
+  int randomNumber = rand() % maxNumber + 1;
+  return randomNumber;
+}
+
+int main(void) {
+  srand(time(NULL));
+  // Modify the code below
+  int randomNumber = getRandomNumber(6);
+  printf("My random number is: %d", randomNumber);
+}

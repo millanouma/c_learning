cc869853f661b8787cfff723affaadc57693ec49
Millan Ouma <26522016+millanouma@users.noreply.github.com>
2023-05-20 23:20:36 +0300

    Create random_number.c

---
+++ random_number.c
@@ -0,0 +1,11 @@
+# This is a simple random number generator for a dice game.
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+  srand(time(NULL));
+  // Write your code below
+  int randomNumber = rand() % 20 + 1;
+  printf("%d\n", randomNumber);
+}
